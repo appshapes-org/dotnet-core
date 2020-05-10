a593ac906b3449f983bda696bfddb85effe7c3dc
Rjae <rjae.easton@gmail.com>
2020-05-10 07:04:15 -0400

    Cover mutations

---
+++ AppShapes.Core.Tests.Unit.Tests/Messaging/TestEventTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using AppShapes.Core.Testing.Messaging;
+using Xunit;
+
+namespace AppShapes.Core.Tests.Unit.Tests.Messaging
+{
+    public class TestEventTests
+    {
+        [Fact]
+        public void GetContextMustReturnExpectedStringWhenCalled()
+        {
+            Assert.Equal("Testing", new StubTestEvent().InvokeGetContext());
+        }
+
+        [Fact]
+        public void GetEntityIdMustReturnTestIdWhenCalled()
+        {
+            TestEvent e = new TestEvent();
+            Assert.Equal(e.TestId, e.GetEntityId());
+        }
+
+        [Fact]
+        public void GetEntityNameMustReturnExpectedStringWhenCalled()
+        {
+            Assert.Equal("Test", new StubTestEvent().InvokeGetEntityName());
+        }
+
+        [Fact]
+        public void GetMessageTypeMustReturnExpectedStringWhenCalled()
+        {
+            Assert.Equal("TestEvent", new StubTestEvent().InvokeGetMessageType());
+        }
+
+        private class StubTestEvent : TestEvent
+        {
+            public IEnumerable<char> InvokeGetContext()
+            {
+                return GetContext();
+            }
+
+            public IEnumerable<char> InvokeGetEntityName()
+            {
+                return GetEntityName();
+            }
+
+            public IEnumerable<char> InvokeGetMessageType()
+            {
+                return GetMessageType();
+            }
+        }
+    }
+}
---
+++ AppShapes.Core.Tests.Unit.Tests/Service/TestStartupBaseTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AppShapes.Core.Testing.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+
+namespace AppShapes.Core.Tests.Unit.Tests.Service
+{
+    public class TestStartupBaseTests
+    {
+        [Fact]
+        public void ConfigureServicesMustAddControllersWhenCalled()
+        {
+            IServiceProvider provider = new TestStartup().ConfigureServices(new ServiceCollection());
+            Assert.NotNull(provider.GetService<IConfigureOptions<MvcOptions>>());
+        }
+
+        [Fact]
+        public void ConfigureServicesMustSetRoutingOptionsToLowercaseUrlsWhenCalled()
+        {
+            IServiceProvider provider = new TestStartup().ConfigureServices(new ServiceCollection());
+            Assert.True(provider.GetRequiredService<IOptions<RouteOptions>>().Value.LowercaseUrls);
+        }
+
+        private class TestStartup : TestStartupBase
+        {
+        }
+    }
+}
