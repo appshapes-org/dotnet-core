ecdad38da6b3be13c447637e98d3700887e86160
Rjae Easton <Rjae@users.noreply.github.com>
2020-05-10 12:46:28 -0400

    Create StringExtensions (#8)

---
+++ AppShapes.Core.Tests.Unit.Tests/Core/StringExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+
+namespace AppShapes.Core.Tests.Unit.Tests.Core
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        [InlineData("\t")]
+        public void WordsFromCamelCaseMustNotReturnWordsWhenValueDoesNotContainWords(string value)
+        {
+            string[] words = value.WordsFromCamelCase();
+            Assert.Empty(words);
+        }
+
+        [Fact]
+        public void WordsFromCamelCaseMustReturnExpectedWordsWhenValueContainsAcronyms()
+        {
+            string[] words = "JSONParser".WordsFromCamelCase();
+            Assert.Equal(2, words.Length);
+            Assert.Equal("JSON", words[0]);
+            Assert.Equal("Parser", words[1]);
+        }
+
+        [Fact]
+        public void WordsFromCamelCaseMustReturnExpectedWordsWhenValueDoesNotContainAcronyms()
+        {
+            string[] words = nameof(WordsFromCamelCaseMustReturnExpectedWordsWhenValueDoesNotContainAcronyms).WordsFromCamelCase();
+            Assert.Equal(14, words.Length);
+            Assert.Equal("From", words[1]);
+            Assert.Equal("Camel", words[2]);
+            Assert.Equal("Case", words[3]);
+        }
+    }
+}
---
+++ AppShapes.Core/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AppShapes.Core
+{
+    public static class StringExtensions
+    {
+        public static string[] WordsFromCamelCase(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value) ? new string[] { } : Regex.Replace(value, @"(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])", " ").Split(" ");
+        }
+    }
+}
