a1bd14da2f673fcc554819c228fdfc836553c49d
Rjae <rjae.easton@gmail.com>
2020-05-21 18:41:38 -0400

    Create KafkaProducerFactory

---
+++ AppShapes.Core.Messaging.Kafka/KafkaProducerFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace AppShapes.Core.Messaging.Kafka
+{
+    public class KafkaProducerFactory
+    {
+        public KafkaProducerFactory(ILogger<KafkaProducer> logger, KafkaSettings settings)
+        {
+            Logger = logger;
+            Settings = settings;
+        }
+
+        public virtual IMessageProducer Create()
+        {
+            return new KafkaProducer(Logger, Settings);
+        }
+
+        private ILogger<KafkaProducer> Logger { get; }
+
+        private KafkaSettings Settings { get; }
+    }
+}
